a63791c61eec9c5dde70ec6f0adbab2cb99fa4be
Roberto Salicio <roberto.salicio@gmail.com>
2008-10-02 12:18:18 +0200

    use 'cms' if Tog::Plugins.settings(:tog_vault, :public_prefix) not set

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,12 @@ with_options(:controller => 'vault/user') do |user|
   user.cms_user_index        'admin/cms/users',                        :action => 'index'
 end
 
+prefix = Tog::Plugins.settings(:tog_vault, :public_prefix) || "cms"
 with_options(:controller => 'vault/site') do |site|
-  site.cms_homepage     Tog::Plugins.settings(:tog_vault, :public_prefix),                :action => 'show_page', :url => '/'
-  site.cms_not_found    Tog::Plugins.settings(:tog_vault, :public_prefix) + '/error/404', :action => 'not_found'
-  site.cms_error        Tog::Plugins.settings(:tog_vault, :public_prefix) + '/error/500', :action => 'error'
-  site.cms_connect      Tog::Plugins.settings(:tog_vault, :public_prefix) + '/*url',      :action => 'show_page'
+  site.cms_homepage     prefix,                :action => 'show_page', :url => '/'
+  site.cms_not_found    prefix + '/error/404', :action => 'not_found'
+  site.cms_error        prefix + '/error/500', :action => 'error'
+  site.cms_connect      prefix + '/*url',      :action => 'show_page'
 end
 
 with_options(:controller => 'vault/mail') do |mail|
