e56864c2ddfda5193f155702dca8618505c50f0a
Roberto Salicio <roberto.salicio@gmail.com>
2009-02-12 18:13:07 +0100

    remove deprecated %d from i18n files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,8 @@ require 'mocha'
 require 'factory_girl'
 require File.expand_path(File.dirname(__FILE__) + '/factories')
 
+begin require 'redgreen'; rescue LoadError; end
+
 class Test::Unit::TestCase
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
