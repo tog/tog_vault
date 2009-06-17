d42e1461e63323a77465ae368f9937f6782a3746
salicio <roberto.salicio@gmail.com>
2009-06-17 06:57:28 -0700

    shoulda 2.10.1 required

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
 require 'test_help'
 require 'test/unit'
-require 'shoulda'
+
+begin
+  gem 'thoughtbot-shoulda', '>=2.10.1'
+  require 'shoulda' 
+rescue Exception => e
+  puts "\n\nYou need shoulda 2.10.1 or greater to test tog_core. Visit http://github.com/thoughtbot/shoulda to view details.\n\n" 
+  exit
+end
+
 require 'mocha' 
 
 require 'factory_girl'
